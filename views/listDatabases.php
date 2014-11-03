105269d9adb96ae9cb82d7b9c78fe5f4c786d5d3
Cyawen <cyawen@outlook.fr>
2014-11-03 22:10:27 +0100

    listDatabases
    listDatabases.php

---
+++ views/listDatabases.php
@@ -0,0 +1,42 @@
+<section>
+
+<fieldset>
+<legend>Liste des bases de données :</legend>
+<form method="post">
+	<select name="database">
+<?php
+foreach ($databases as $d) {
+	
+	/*foreach ($liaisons as $l) {
+		$vd = $this->vm->get($l->villeDepart());
+		$va = $this->vm->get($l->villeArrivee());
+		echo '<option value="'.$l->id().'"';
+		if ($l->id() == $t->liaison()) {
+			echo ' selected="selected"';
+		}
+		echo '>'.$vd->nom().'->'.$va->nom().' ('.$l->longueur().'km)</option>';
+	}*/
+	echo '<option value="'.$d->db_id().'"';
+		echo '>Nom : '.$d->db_name().'</option>';
+	
+}
+?>
+			</select>
+		<input type="button" value="Afficher" onclick="deleteButtonClicked(event)" />
+	</form>
+</fieldset>
+
+<script>
+function modifyButtonClicked(e) {
+	var f = e.target.parentNode;
+	f.action = 'index.php?section=trajet&action=update';
+	f.submit();
+}
+function deleteButtonClicked(e) {
+	var f = e.target.parentNode;
+	f.action = 'index.php?section=database&action=show';
+	f.submit();
+}
+</script>
+
+</section>
